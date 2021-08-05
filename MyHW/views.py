c61bcb4e42980a2085cfc6648804fdd14397587f
Georgiy Neizvestny <83881979+Egorius96@users.noreply.github.com>
2021-08-05 16:28:20 +0300

    Update views.py
    added strptime & strftime functions from datetime module

--- MyHW/views.py
+++ MyHW/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def hw1901(request):
     messages = {request.POST.get('message01', ''):len(request.POST.get('message01', '')),
@@ -8,11 +9,18 @@ def hw1901(request):
     return render(request, 'HW_1901.html', context={'result':keymax})
 
 def hw1902(request):
-    date = request.POST.get('date', '')
-    if date[5:7] == '01' and date[8:] == '01':
-        date = f'С новым {date[0:4]} годом!'
-    return render(request, 'HW_1902.html', context={'date':date})
-
+    if request.method == 'GET':
+        date = request.POST.get('date', '')
+        return render(request, 'HW_1902.html', context={'date': date})
+    elif request.method == 'POST':
+        date, format = request.POST.get('date', ''), '%Y-%m-%d'
+        date = datetime.strptime(date, format)
+        if date.month == 1 and date.day == 1:
+            date = f'С новым {date.year} годом!'
+            return render(request, 'HW_1902.html', context={'date': date})
+        else:
+            return render(request, 'HW_1902.html', context={'date':date.strftime('%Y-%m-%d')})
+    
 def additionaltask1(request):
     req = request.POST.get('username', 'World')
     if req == '':
