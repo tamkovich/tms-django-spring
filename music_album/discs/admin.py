c7070aa1b7094228a6068cfc6adbdf5dce55b4ea
KarolinaLeonovich <karolinaleonovich@gmail.com>
2021-08-23 21:42:15 +0300

    Addind filters by __

--- music_album/discs/admin.py
+++ music_album/discs/admin.py
@@ -9,10 +9,19 @@ class MusicBandAdmin(admin.ModelAdmin):
 
 @admin.register(Track)
 class TrackAdmin(admin.ModelAdmin):
-    list_display = ('name', 'duration', 'music_album')
-    list_filter = ('music_album',)
+    list_display = ('name', 'duration', 'music_album')  # чтобы в админке сразу таблицой показывало 'duration', 'music_album'
+    #fields = ['name', ('duration', 'music_album')]  # те что в кортеже при создании нового трека в одминке будут горизонтально
+    list_filter = ('music_album', 'music_album__artist__name')
+    fieldsets = (                                    #   не получается вместе с fields
+        (None, {
+            'fields': ('name',)
+        }),
+        ('Detailed information', {
+            'fields': ('duration', 'music_album')
+        }),
+    )
 
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ('name', 'age', 'artist')
-    list_filter = ('artist',)
+    list_filter = ('artist', 'artist__age')
